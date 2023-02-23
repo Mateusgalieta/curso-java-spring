3832dbf283b7e54df3efef65140a2c8d014401f7
Mateus Galieta <mateusgalieta14@gmail.com>
2023-02-23 20:09:12 -0300

    Entities Class and Properties Insertion

---
+++ src/main/java/com/mateusgalieta/bookstoragemanager/entity/Author.java
@@ -0,0 +1,16 @@
+package com.mateusgalieta.bookstoragemanager.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
